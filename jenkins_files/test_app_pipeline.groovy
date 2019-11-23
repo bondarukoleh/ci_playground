c4264d9494ca11850096bd51c12607a03316ad8d
bondaruk9000@gmail.com <bondaruk9000@gmail.com>
2019-11-23 14:16:54 +0200

    deleted extra quote type

--- jenkins_files/test_app_pipeline.groovy
+++ jenkins_files/test_app_pipeline.groovy
@@ -6,14 +6,14 @@ node {
      currentCommitId = readFile('.git/currentCommitId').trim()
    }
 
-   stage('test') {
+   stage('Test') {
      nodejs(nodeJSInstallationName: 'nodejs') {
        sh 'npm install'
        sh 'npm test'
      }
    }
 
-   stage('docker build/push') {
+   stage('Docker build/push') {
      /*https://index.docker.io/v1/ - It"s a dockerhub, 'dockerhub' - cred id that we have in jenkins*/
      docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
        docker.build("bondarukoleh/docker_test_app:${currentCommitId}", '.').push() /*. path where Docker file is*/

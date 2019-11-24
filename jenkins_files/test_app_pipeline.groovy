6d70ab2ebb9408df1b6330ef85f9025c1d72fb97
bondaruk9000@gmail.com <bondaruk9000@gmail.com>
2019-11-24 21:17:39 +0200

    added slack notification integration

--- jenkins_files/test_app_pipeline.groovy
+++ jenkins_files/test_app_pipeline.groovy
@@ -2,21 +2,30 @@ node {
    def currentCommitId
    stage('Preparation') {
      checkout scm
-     sh "git rev-parse --short HEAD > .git/currentCommitId"                        
+     sh "git rev-parse --short HEAD > .git/currentCommitId"
      currentCommitId = readFile('.git/currentCommitId').trim()
    }
 
-   stage('Test') {
+try{
+	stage('Test') {
      nodejs(nodeJSInstallationName: 'nodejs') {
        sh 'npm install'
        sh 'npm test'
      }
+		 /*Send notification to slack*/
+		slackSend (color: '#057d0b', message: "Test stage passed: '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
    }
+} catch(e) {
+		def name = env.JOB_NAME
+		def num = env.BUILD_NUMBER
+		def url = env.BUILD_URL
+		slackSend (color: '#FF0000', message: "Failed Custom stage: '${name} [${num}]' (${url}) \n Error: ${e}")
+}
 
-   stage('Docker build/push') {
-     /*https://index.docker.io/v1/ - It"s a dockerhub, 'dockerhub' - cred id that we have in jenkins*/
-     docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
-       docker.build("bondarukoleh/docker_test_app:${currentCommitId}", '.').push() /*. path where Docker file is*/
-     }
-   }
+  //  stage('Docker build/push') {
+  //    /*https://index.docker.io/v1/ - It"s a dockerhub, 'dockerhub' - cred id that we have in jenkins*/
+  //    docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
+  //      docker.build("bondarukoleh/docker_test_app:${currentCommitId}", '.').push() /*. path where Docker file is*/
+  //    }
+  //  }
 }

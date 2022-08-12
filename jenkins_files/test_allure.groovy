8f7bb83a4b4129760e1ffd873809509bdddef689
bondaruk9000@gmail.com <bondaruk9000@gmail.com>
2022-08-12 18:11:14 +0300

    added allure

---
+++ jenkins_files/test_allure.groovy
@@ -0,0 +1,21 @@
+job("Test Allure") {
+
+  /* Software configuration management. User and email - are mandatory */
+  scm {
+    git("https://github.com/bondarukoleh/ci_playground") { node ->
+      node / gitConfigName("Oleh Bondaruk DSL")
+      node / gitConfigEmail("bondaruk9000@gmail.com")
+    }
+  }
+
+  wrappers {
+    /* this is the name of the NodeJS installation in Jenkins, Manage -> Configure Tools ->
+     NodeJS installations -> Name */
+    nodejs("nodejs")
+  }
+
+	step('Testing') {
+		sh 'npm i --no-audit'
+		sh 'npm t'
+  }
+}
